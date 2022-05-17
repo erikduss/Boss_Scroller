bb1f1e0b8a0aa360eb21244ac82abcb07fdcb118
Erikduss <erik.dusschooten@student.hku.nl>
2022-05-17 18:19:10 +0200

    Fixed Aspect Ratio

--- Project_Vrij_2/Boss_Scroller/Assets/Scripts/SmoothCamera.cs
+++ Project_Vrij_2/Boss_Scroller/Assets/Scripts/SmoothCamera.cs
@@ -26,7 +26,7 @@ public class SmoothCamera : MonoBehaviour
             Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
             Vector3 destination = transform.position + delta;
 
-            if (destination.y < 0) destination.y = 0;
+            if (destination.y < 1) destination.y = 1;
             if (destination.x < -30f) destination.x = -30f;
             if (destination.x > 0f) destination.x = 0f;
 
